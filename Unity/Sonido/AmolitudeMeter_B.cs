717eba731f308f77ffaab5d943dd5bd63b0371cf
Andres <andrespablo994@gmail.com>
2020-08-28 12:25:48 -0300

    Create AmolitudeMeter_B.cs

---
+++ Unity/Sonido/AmolitudeMeter_B.cs
@@ -0,0 +1,34 @@
+   public float sensitivity = 100;
+   public float loudness = 0;
+   private AudioSource _audio;
+   void Awake()
+   {
+       _audio = GetComponent<AudioSource>();
+   }
+   void Start()
+   {
+       _audio.clip = Microphone.Start(null, true, 10, 44100);
+       _audio.loop = true;
+       _audio.mute = false;
+       while (!(Microphone.GetPosition(null) > 0)) { }
+       _audio.Play();
+   }
+   void Update()
+   {
+       loudness = GetAveragedVolume() * sensitivity;
+       if(loudness > 1)
+       {
+          //DO SOMETHING
+       }
+   }
+   float GetAveragedVolume()
+   {
+       float[] data = new float[256];
+       float a = 0;
+       _audio.GetOutputData(data, 0);
+       foreach (float s in data)
+       {
+           a += Mathf.Abs(s);
+       }
+       return a / 256;
+   }
